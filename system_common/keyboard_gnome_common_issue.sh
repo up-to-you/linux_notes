6b9dc639ea704dc83880f60e8d941a5d52032292
up-to-you <artursabirovdev@gmail.com>
2018-05-14 18:28:36 +0300

    Create keyboard_gnome_common_issue.sh

---
+++ system_common/keyboard_gnome_common_issue.sh
@@ -0,0 +1,8 @@
+# disable default ctrl + alt + arrow gnome shortcuts (workspaces switcher) for navigation in other apps
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left "['']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right "['']"
+
+# patched xorg for keymap <-> source switcher keys combinations:
+# https://launchpad.net/~nrbrtx/+archive/ubuntu/xorg-hotkeys
+# e.g. gnome-tweak-tools : switch source - alt + shift
+sudo add-apt-repository ppa:nrbrtx/xorg-hotkeys
