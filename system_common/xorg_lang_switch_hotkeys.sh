45bea27459d30ef06ebba957662675c543a1ee1d
up-to-you <artursabirovdev@gmail.com>
2018-05-16 10:22:29 +0300

    Create xorg_lang_switch_hotkeys.sh

---
+++ system_common/xorg_lang_switch_hotkeys.sh
@@ -0,0 +1,4 @@
+# works for Ubuntu 18.04
+# patched xorg for fix of language source switch compatability bug like alt/shift, ctrl/shift etc.
+sudo add-apt-repository ppa:nrbrtx/xorg-hotkeys
+sudo apt update && sudo apt upgrade
