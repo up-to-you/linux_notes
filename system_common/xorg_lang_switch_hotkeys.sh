80a38132bc4636970b2a2c72a2291bcb753cf17e
up-to-you <artursabirovdev@gmail.com>
2018-05-16 10:28:57 +0300

    Update xorg_lang_switch_hotkeys.sh

--- system_common/xorg_lang_switch_hotkeys.sh
+++ system_common/xorg_lang_switch_hotkeys.sh
@@ -2,3 +2,6 @@
 # patched xorg for fix of language source switch compatability bug like alt/shift, ctrl/shift etc.
 sudo add-apt-repository ppa:nrbrtx/xorg-hotkeys
 sudo apt update && sudo apt upgrade
+
+sudo apt remove xserver-xorg && sudo apt install xserver-xorg
+sudo dpkg-reconfigure xserver-xorg
