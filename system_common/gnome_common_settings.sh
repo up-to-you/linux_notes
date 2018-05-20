3a87a59cc00939257683c880a2f6052e73f48b62
up-to-you <artursabirovdev@gmail.com>
2018-05-20 14:55:44 +0300

    Delete gnome_common_settings.sh

--- system_common/gnome_common_settings.sh
+++
@@ -1,6 +0,0 @@
-
-# fix for forward / backward keys in intellij idea
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-left "['']"
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-right "['']"
-
-
