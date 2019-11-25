7e582182bdbbe2b375a74cdb9120a0ccc4d0947b
up-to-you <uptoyou.git@gmail.com>
2019-11-25 23:42:39 +0300

    Update tlp.sh

--- dell_5580/tlp.sh
+++ dell_5580/tlp.sh
@@ -1,5 +1,5 @@
 sudo nano /etc/default/tlp
 
-# since tlp intersects with bbswitch and nvidia-off.service
+# since tlp conflicts/intersects with bbswitch and nvidia-off.service
 ...
 RUNTIME_PM_DRIVER_BLACKLIST="nouveau nvidia"
