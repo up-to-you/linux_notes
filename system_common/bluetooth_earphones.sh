2505be87cc34e2cdc595edb0b69be26a6434bdd2
up-to-you <uptoyou.git@gmail.com>
2020-01-20 13:48:07 +0300

    Create bluetooth_earphones.sh

---
+++ system_common/bluetooth_earphones.sh
@@ -0,0 +1,17 @@
+sudo apt install --reinstall pulseaudio-module-bluetooth
+
+nano /etc/pulse/default.pa
+nano /etc/pulse/system.pa
+
+#.ifexists module-bluetooth-policy.so
+#load-module module-bluetooth-policy
+#.endif
+
+#.ifexists module-bluetooth-discover.so
+#load-module module-bluetooth-discover
+#.endif
+
+# ======>>>
+
+load-module module-bluetooth-policy
+load-module module-bluetooth-discover
