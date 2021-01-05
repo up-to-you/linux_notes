29e05fcd746795e683c87a2fddcfad3944e28137
up-to-you <uptoyou.git@gmail.com>
2021-01-06 01:47:42 +0300

    Update commons.sh

--- mac/commons.sh
+++ mac/commons.sh
@@ -1,4 +1,6 @@
 # actually, the values lower than min works in some apps (need check, catalina 10.15.7)
-sudo defaults write NSGlobalDomain KeyRepeat -float 0.7
-defaults write -g KeyRepeat -float 0.7
+# sudo defaults write NSGlobalDomain KeyRepeat -float 0.7
+# defaults write -g KeyRepeat -float 0.7
+sudo defaults write NSGlobalDomain KeyRepeat -int 1
+defaults write -g KeyRepeat -int 1
 defaults write -g InitialKeyRepeat -int 9
